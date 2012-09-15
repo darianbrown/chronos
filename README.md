21f5c7da79dcdcfc04ca4ba7e6245a1e856ac029
Darian Brown <darianbr@gmail.com>
2012-09-15 16:27:07 +1000

    Added base methods
    Added methods such as resolve, timestamp and ymd.

--- README.md
+++ README.md
@@ -3,12 +3,36 @@
 Chronos is a simple abstraction layer for PHP's date and time functions with the aim of standardising using dates and times within an application.
 
 ## Usage
+
+### Creating a new instance of Chronos
 ```php
 <?php
 
-// TODO
+use Succinct\Chronos\Chronos;
+
+// create a Chronos instance with date reference of yesterday
+$chronos = Chronos::yesterday();
+
+// create a Chronos instance with date reference of tomorrow
+$chronos = Chronos::tomorrow();
+
+// create a Chronos instance with date reference of today
+$chronos = Chronos::today();
+
+$chronos = new Chronos();
 ````
 
-### sub sub heading
+### Displaying a date
+```php
+<?php
 
-Sample
+// echos Y-m-d (e.g. 2012-02-12)
+echo $chronos->ymd() . PHP_EOL;
+
+// echos Y-m-d (e.g. 2012/02/12)
+echo $chronos->ymd('/') . PHP_EOL;
+
+// displays current unix timestamp (e.g. 1347690188)
+echo $chronos->timestamp() . PHP_EOL;
+
+```
--- src/Succinct/Chronos/Chronos.php
+++ src/Succinct/Chronos/Chronos.php
@@ -3,9 +3,46 @@
 namespace Succinct\Chronos;
 
 class Chronos {
+
+	protected $date = NULL;
 	
-	public function __construct() {
-		echo 'Welcome to chronos!' . PHP_EOL;
+	public function __construct($date=NULL) {
+		$this->timestamp = $this->resolve($date);
+	}
+
+	/**
+	 * @returns int
+	 */
+	public function timestamp() {
+		return $this->timestamp;
+	}
+
+	public function ymd($delimiter='-') {
+		return date('Y' . $delimiter . 'm' . $delimiter . 'd', $this->timestamp);
+	}
+
+	static public function today() {
+		return new self();
+	}
+
+	static public function tomorrow() {
+		return new self('+1 day');
+	}
+
+	static public function yesterday() {
+		return new self('-1 day');
+	}
+
+	protected function resolve($date) {
+		if ($result = @strtotime($date)) {
+			return $result;
+		} else if ($date instanceof Chronos) {
+			return $date->timestamp();
+		} else if ($date === NULL) {
+			return time();
+		}
+
+		throw new \InvalidArgumentException('Expected an instance of Chronos or a date string. Received: ' . $date);
 	}
 
 }
