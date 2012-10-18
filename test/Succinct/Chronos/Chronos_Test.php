588200eee65850ac90b712d7cc8c8eaa21b8545e
Darian Brown <darianbr@gmail.com>
2012-10-18 14:03:29 +1000

    Added unit tests

--- test/Succinct/Chronos/Chronos_Test.php
+++ test/Succinct/Chronos/Chronos_Test.php
@@ -4,10 +4,54 @@ use Succinct\Chronos\Chronos;
 
 class Chronos_Test extends PHPUnit_Framework_TestCase {
 
-	public function testTFW() {
-		$db = new Chronos();
-		$this->assertEquals(2,1);
+	protected $chronos = NULL;
 
+	protected function setUp() {
+		parent::setUp();
+		$this->chronos = new Chronos('2010-09-08 17:16:15');
+	}
+
+	/**
+	 * @dataProvider ymd_data
+	 */
+	public function testYmd($delimiter, $expected) {
+		$date = $this->chronos->ymd($delimiter);
+		$this->assertSame($date, $expected);
+	}
+
+	/**
+	 * @dataProvider his_data
+	 */
+	public function testHis($delimiter, $expected) {
+		$date = $this->chronos->his($delimiter);
+		$this->assertSame($date, $expected);
+	}
+
+	public function testDay() {
+		$date = $this->chronos->day();
+		$this->assertSame($date, 'Wed');
+	}
+
+
+	public function ymd_data() {
+		return array(
+			array(NULL, '2010-09-08')
+			, array('-', '2010-09-08')
+			, array('/', '2010/09/08')
+			, array(' ', '2010 09 08')
+			, array(FALSE, '20100908')
+		);
+	}
+
+
+	public function his_data() {
+		return array(
+			array(NULL, '17:16:15')
+			, array('-', '17-16-15')
+			, array('/', '17/16/15')
+			, array(' ', '17 16 15')
+			, array(FALSE, '171615')
+		);
 	}
 
 }
