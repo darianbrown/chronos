0390e65793d2414105503235c5894ad5ad0160b9
darian <darianbr@gmail.com>
2013-05-07 12:48:53 +1000

    Code style fix after php-csfixer

--- src/Succinct/Chronos/Chronos.php
+++ src/Succinct/Chronos/Chronos.php
@@ -4,153 +4,154 @@ namespace Succinct\Chronos;
 
 class Chronos
 {
+    public function __construct($date=NULL)
+    {
+        $this->timestamp = $this->resolve($date);
+    }
 
-	protected $date = NULL;
+    public static function today()
+    {
+        return new self();
+    }
 
-	public function __construct($date=NULL)
-	{
-		$this->timestamp = $this->resolve($date);
-	}
+    public static function tomorrow()
+    {
+        return new self('+1 day');
+    }
 
-	static public function today()
-	{
-		return new self();
-	}
+    public static function yesterday()
+    {
+        return new self('-1 day');
+    }
 
-	static public function tomorrow()
-	{
-		return new self('+1 day');
-	}
+    public static function now()
+    {
+        return new self;
+    }
 
-	static public function yesterday()
-	{
-		return new self('-1 day');
-	}
+    public function timestamp()
+    {
+        return $this->timestamp;
+    }
 
-	static public function now()
-	{
-		return new self;
-	}
+    public function ymd($delimiter='-')
+    {
+        $delimiter = $delimiter === NULL ? '-' : $delimiter;
 
-	public function timestamp()
-	{
-		return $this->timestamp;
-	}
+        return date('Y' . $delimiter . 'm' . $delimiter . 'd', $this->timestamp);
+    }
 
-	public function ymd($delimiter='-')
-	{
-		$delimiter = $delimiter === NULL ? '-' : $delimiter;
-		return date('Y' . $delimiter . 'm' . $delimiter . 'd', $this->timestamp);
-	}
+    // TODO: Add 24/12 hour output
+    public function his($delimiter=':')
+    {
+        $delimiter = $delimiter === NULL ? ':' : $delimiter;
 
-	// TODO: Add 24/12 hour output
-	public function his($delimiter=':')
-	{
-		$delimiter = $delimiter === NULL ? ':' : $delimiter;
-		return date('H' . $delimiter . 'i' . $delimiter . 's', $this->timestamp);
-	}
+        return date('H' . $delimiter . 'i' . $delimiter . 's', $this->timestamp);
+    }
 
-	// TODO: long and short (Wed and Wednesday)
-	public function day()
-	{
-		return date('D', $this->timestamp);
-	}
+    // TODO: long and short (Wed and Wednesday)
+    public function day()
+    {
+        return date('D', $this->timestamp);
+    }
 
-	/**
-	 * Get day of year
-	 */
-	public function doy()
-	{
-		return date('z', $this->datetime);
-	}
+    /**
+     * Get day of year
+     */
+    public function doy()
+    {
+        return date('z', $this->datetime);
+    }
 
-	/**
-	 * Get day of week
-	 */
-	public function dow()
-	{
-		return $this->format('w');
-	}
+    /**
+     * Get day of week
+     */
+    public function dow()
+    {
+        return $this->format('w');
+    }
 
-	public function get_week()
-	{
-		return $this->format('W');
-	}
+    public function get_week()
+    {
+        return $this->format('W');
+    }
 
-	/**
-	 * Displays sting showing how long ago the date instance is from now
-	 */
-	public function ago($abbr=FALSE)
-	{
-		$timestamp = $this->timestamp();
+    /**
+     * Displays sting showing how long ago the date instance is from now
+     */
+    public function ago($abbr=FALSE)
+    {
+        $timestamp = $this->timestamp();
 
-		// figure out how many seconds the date is from now
-		$seconds = time() - $timestamp;
+        // figure out how many seconds the date is from now
+        $seconds = time() - $timestamp;
 
-		// in the future
-		if( $seconds < 0 ) {
-			$f = ($seconds*-1) . " seconds from now";
-		}
-		// 0 - 9 seconds
-		else if( $seconds < 10 ) {
-			$f = "a few moments ago";
-		}
-		// 10 - 59 seconds
-		else if( $seconds < 60 ) {
-			$f = "$seconds seconds ago";
-		}
-		// 1 minute - 59 minutes
-		else if( $seconds < 3600 ) {
-			$f = floor($seconds/60)." minutes ago";
-		}
-		// 1 hour - 23 hours
-		else if( $seconds < 86400 ) {
-			$f = floor($seconds/3600)." hours ago";
-		}
-		// 24 hours - 47 hours (yesterday)
-		else if( $seconds < 172800 ) {
-			$f = "Yesterday";
-		}
-		// otherwise
-		else {
-			// morning of 6 days ago (i.e. if today is fri, this will be the date of previous sat morning 00:00:00)
-			// this prevents us displaying fri (for a date of last week) when today is fri.
-			$mk = strtotime('-6 days');
-			$mk = mktime(00,00,00,date('m',$mk),date('d',$mk),date('Y',$mk));
+        // in the future
+        if ($seconds < 0) {
+            $f = ($seconds*-1) . " seconds from now";
+        }
+        // 0 - 9 seconds
+        else if ($seconds < 10) {
+            $f = "a few moments ago";
+        }
+        // 10 - 59 seconds
+        else if ($seconds < 60) {
+            $f = "$seconds seconds ago";
+        }
+        // 1 minute - 59 minutes
+        else if ($seconds < 3600) {
+            $f = floor($seconds/60)." minutes ago";
+        }
+        // 1 hour - 23 hours
+        else if ($seconds < 86400) {
+            $f = floor($seconds/3600)." hours ago";
+        }
+        // 24 hours - 47 hours (yesterday)
+        else if ($seconds < 172800) {
+            $f = "Yesterday";
+        }
+        // otherwise
+        else {
+            // morning of 6 days ago (i.e. if today is fri, this will be the date of previous sat morning 00:00:00)
+            // this prevents us displaying fri (for a date of last week) when today is fri.
+            $mk = strtotime('-6 days');
+            $mk = mktime(00,00,00,date('m',$mk),date('d',$mk),date('Y',$mk));
 
-			// 48 hours - 7 days (show day - Friday, Saturday, etc.)
-			if( $mk < $timestamp ) {
-				$f = date('l',$timestamp);
-			}
-			// more than 7 days ago (show date - 22 September)
-			else {
-				// now if the date is in a previous year, we add the year to the end
-				if( date('Y') != date('Y',$timestamp) ) {
-					$f = date('d F Y',$timestamp);
-				}
-				// else return the date without the year
-				else {
-					$f = date('d F',$timestamp);
-				}
-			}
-		}
+            // 48 hours - 7 days (show day - Friday, Saturday, etc.)
+            if ($mk < $timestamp) {
+                $f = date('l',$timestamp);
+            }
+            // more than 7 days ago (show date - 22 September)
+            else {
+                // now if the date is in a previous year, we add the year to the end
+                if ( date('Y') != date('Y',$timestamp) ) {
+                    $f = date('d F Y',$timestamp);
+                }
+                // else return the date without the year
+                else {
+                    $f = date('d F',$timestamp);
+                }
+            }
+        }
 
-		return ($abbr) ? "<abbr title='".date('d F Y h:i A', $timestamp)."'>$f</abbr>" : $f;
-	}
+        return ($abbr) ? "<abbr title='".date('d F Y h:i A', $timestamp)."'>$f</abbr>" : $f;
+    }
 
-	protected function resolve($date)
-	{
-		if ($date === NULL) {
-			return time();
-		} else if ($date instanceof Chronos) {
-			return $date->timestamp();
-		} else if ($result = @strtotime($date)) {
-			return $result;
-		} else if ($date === (int) $date) {
-			return $date;
-		}
+    protected function resolve($date)
+    {
+        if ($date === NULL) {
+            return time();
+        } elseif ($date instanceof Chronos) {
+            return $date->timestamp();
+        } elseif ($result = @strtotime($date)) {
+            return $result;
+        } elseif ($date === (int) $date) {
+            return $date;
+        }
 
-		throw new \InvalidArgumentException('Expected an instance of Chronos or a date string. Received: ' . $date);
-	}
+        throw new \InvalidArgumentException('Expected an instance of Chronos or a date string. Received: ' . $date);
+    }
+
+    protected $timestamp = NULL;
 
 }
--- test/suite/Succinct/Chronos/Chronos_Test.php
+++ test/suite/Succinct/Chronos/Chronos_Test.php
@@ -7,95 +7,93 @@ use PHPUnit_Framework_TestCase;
 class Chronos_Test extends PHPUnit_Framework_TestCase
 {
 
-	protected $chronos = NULL;
+    protected $chronos = NULL;
 
-	protected function setUp()
-	{
-		parent::setUp();
+    protected function setUp()
+    {
+        parent::setUp();
 
-		$this->chronos = new Chronos('2010-09-08 17:16:15');
-	}
+        $this->chronos = new Chronos('2010-09-08 17:16:15');
+    }
 
-	public function testConstructor()
-	{
-		// TODO: Test contstructor by passing time string, chronos object and unixtimestamp
-		$chronos = new Chronos();
+    public function testConstructor()
+    {
+        // TODO: Test contstructor by passing time string, chronos object and unixtimestamp
+        $chronos = new Chronos();
 
-		$this->assertInstanceOf(__NAMESPACE__ . "\Chronos", $chronos);
-	}
+        $this->assertInstanceOf(__NAMESPACE__ . "\Chronos", $chronos);
+    }
 
-	public function testToday()
-	{
-		$chronos = Chronos::today();
-		$this->assertSame($chronos->ymd(), date('Y-m-d'));
-	}
+    public function testToday()
+    {
+        $chronos = Chronos::today();
+        $this->assertSame($chronos->ymd(), date('Y-m-d'));
+    }
 
-	public function testTomorrow()
-	{
-		$chronos = Chronos::tomorrow();
-		$this->assertSame($chronos->ymd(), date('Y-m-d', strtotime('+1 day')));
-	}
+    public function testTomorrow()
+    {
+        $chronos = Chronos::tomorrow();
+        $this->assertSame($chronos->ymd(), date('Y-m-d', strtotime('+1 day')));
+    }
 
-	public function testYesterday()
-	{
-		$chronos = Chronos::yesterday();
-		$this->assertSame($chronos->ymd(), date('Y-m-d', strtotime('-1 day')));
-	}
+    public function testYesterday()
+    {
+        $chronos = Chronos::yesterday();
+        $this->assertSame($chronos->ymd(), date('Y-m-d', strtotime('-1 day')));
+    }
 
-	public function testTimestamp()
-	{
-		$timestamp = strtotime('2010-09-08 17:16:15');
-		$chronos = new Chronos('2010-09-08 17:16:15');
+    public function testTimestamp()
+    {
+        $timestamp = strtotime('2010-09-08 17:16:15');
+        $chronos = new Chronos('2010-09-08 17:16:15');
 
-		$this->assertSame($timestamp, $chronos->timestamp());
-	}
+        $this->assertSame($timestamp, $chronos->timestamp());
+    }
 
-	/**
-	 * @dataProvider ymd_data
-	 */
-	public function testYmd($delimiter, $expected)
-	{
-		$date = $this->chronos->ymd($delimiter);
-		$this->assertSame($date, $expected);
-	}
+    /**
+     * @dataProvider ymd_data
+     */
+    public function testYmd($delimiter, $expected)
+    {
+        $date = $this->chronos->ymd($delimiter);
+        $this->assertSame($date, $expected);
+    }
 
-	/**
-	 * @dataProvider his_data
-	 */
-	public function testHis($delimiter, $expected)
-	{
-		$date = $this->chronos->his($delimiter);
-		$this->assertSame($date, $expected);
-	}
+    /**
+     * @dataProvider his_data
+     */
+    public function testHis($delimiter, $expected)
+    {
+        $date = $this->chronos->his($delimiter);
+        $this->assertSame($date, $expected);
+    }
 
-	public function testDay()
-	{
-		$date = $this->chronos->day();
-		$this->assertSame($date, 'Wed');
-	}
+    public function testDay()
+    {
+        $date = $this->chronos->day();
+        $this->assertSame($date, 'Wed');
+    }
 
+    public function ymd_data()
+    {
+        return array(
+            array(NULL, '2010-09-08')
+            , array('-', '2010-09-08')
+            , array('/', '2010/09/08')
+            , array(' ', '2010 09 08')
+            , array(FALSE, '20100908')
+        );
+    }
 
-	public function ymd_data()
-	{
-		return array(
-			array(NULL, '2010-09-08')
-			, array('-', '2010-09-08')
-			, array('/', '2010/09/08')
-			, array(' ', '2010 09 08')
-			, array(FALSE, '20100908')
-		);
-	}
-
-
-	public function his_data()
-	{
-		return array(
-			array(NULL, '17:16:15')
-			, array('-', '17-16-15')
-			, array('/', '17/16/15')
-			, array(' ', '17 16 15')
-			, array(FALSE, '171615')
-		);
-	}
+    public function his_data()
+    {
+        return array(
+            array(NULL, '17:16:15')
+            , array('-', '17-16-15')
+            , array('/', '17/16/15')
+            , array(' ', '17 16 15')
+            , array(FALSE, '171615')
+        );
+    }
 
 }
